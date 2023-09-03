ae136ba6c077f4bae55f526a3e5535af49b93356
JonjimenezF <jon.jimenez@duocuc.cl>
2023-09-03 10:06:55 -0300

    primer commit con una pagina login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
 ];
