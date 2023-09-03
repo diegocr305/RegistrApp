4624e422e3ababfafde98a5e5967189a554a5747
Diego Cabrera <d.cabrera@duocuc.cl>
2023-09-03 13:28:41 -0300

    Login Alumno y profesor 2

--- src/app/loginprofesor/loginprofesor.page.ts
+++ src/app/loginprofesor/loginprofesor.page.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Profesor } from '../models/profesor';
 
 @Component({
   selector: 'app-loginprofesor',
   templateUrl: './loginprofesor.page.html',
   styleUrls: ['./loginprofesor.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LoginprofesorPage implements OnInit {
 
-  constructor() { }
+  ListProfesores: Profesor[] = [
+    new Profesor('12345678-k', 'Juan', 'Perez', '20/03/1970', 'Matemáticas', 'ju.perez@duocuc.cl', '123'),
+    new Profesor('87654321-k', 'Maria', 'Gonzalez', '25/12/1975', 'Física', 'ma.gonzalez@duocuc.cl', '123'),
+  ];
+
+  user = {
+    usuario: "",
+    password: ""
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  ingresar() {
+    for (let i = 0; i < this.ListProfesores.length; i++) {
+      if (this.ListProfesores[i].correoElectronico === this.user.usuario && this.ListProfesores[i].contrasena == this.user.password) {
+        console.log(this.ListProfesores[i]);
+        this.router.navigate(['/home']);
+      }
+    }
+  }
 }
