37ce1511af17f02a88d56e111cb5d71c2540c5e5
Daniela Poblete meza <da.pobletem@duocuc.cl>
2023-09-04 12:37:28 -0300

    cambio perfil alumnos

--- src/app/perfil-alumno/perfil-alumno.page.ts
+++ src/app/perfil-alumno/perfil-alumno.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-alumno',
@@ -11,10 +12,13 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class PerfilAlumnoPage implements OnInit {
-
-  constructor() { }
+  userInfo: any | undefined;
+  constructor( private router: Router, private activateRoute: ActivatedRoute) {
+    this.userInfo = this.router.getCurrentNavigation ()?.extras.state?.['user'];
+   }
 
   ngOnInit() {
+    console.log(this.userInfo)
   }
 
 }
