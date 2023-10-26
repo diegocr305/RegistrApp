afa21e08b45f26f8754e7c80a1c9036aacc9856b
Diego Cabrera <d.cabrera@duocuc.cl>
2023-10-25 23:42:27 -0300

    Service Profesor y login profesor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,33 +12,29 @@ export const routes: Routes = [
   },
   {
     path: 'recuperarcontrasena',
-    loadComponent: () => import('./recuperarcontrasena/recuperarcontrasena.page').then( m => m.RecuperarcontrasenaPage)
+    loadComponent: () => import('./recuperarcontrasena/recuperarcontrasena.page').then(m => m.RecuperarcontrasenaPage)
   },
   {
     path: 'loginprofesor',
-    loadComponent: () => import('./loginprofesor/loginprofesor.page').then( m => m.LoginprofesorPage)
+    loadComponent: () => import('./loginprofesor/loginprofesor.page').then(m => m.LoginprofesorPage)
   },
   {
     path: 'perfil-profesor',
-    loadComponent: () => import('./perfil-profesor/perfil-profesor.page').then( m => m.PerfilProfesorPage)
+    loadComponent: () => import('./perfil-profesor/perfil-profesor.page').then(m => m.PerfilProfesorPage)
   },
   {
     path: 'perfil-alumno',
-    loadComponent: () => import('./perfil-alumno/perfil-alumno.page').then( m => m.PerfilAlumnoPage)
+    loadComponent: () => import('./perfil-alumno/perfil-alumno.page').then(m => m.PerfilAlumnoPage)
   },
   {
     path: 'login-alumno',
-    loadComponent: () => import('./login-alumno/login-alumno.page').then( m => m.LoginAlumnoPage)
+    loadComponent: () => import('./login-alumno/login-alumno.page').then(m => m.LoginAlumnoPage)
   },
   {
 
-    path: 'vista-profe',
-    loadComponent: () => import('./vista-profe/vista-profe.page').then( m => m.VistaProfePage)
-  },{
 
-  
     path: 'admin',
-    loadComponent: () => import('./admin/admin.page').then( m => m.AdminPage)
+    loadComponent: () => import('./admin/admin.page').then(m => m.AdminPage)
   },
 
 
